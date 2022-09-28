06dcc44b28abb570250d853124415c605eb6c05f
Emir Keskin <emir04kes@hotmail.com>
2022-09-28 15:16:48 +0300

    push

--- scripts/job.js
+++ scripts/job.js
@@ -7,3 +7,8 @@ const refer1 = document.querySelector('span.more1')
 function opening1() {
   refer1.classList.toggle('visible')
 }
+
+const refer2 = document.querySelector('span.more2')
+function opening2() {
+  refer2.classList.toggle('visible')
+}
