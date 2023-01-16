b930a9492982fadeb9085d03c3e39d7216dd28ba
Wragnardev <shalithamza@gmail.com>
2023-01-16 19:57:54 +0300

    push

---
+++ src/script/main.js
@@ -0,0 +1,7 @@
+navigation = document.getElementById("navigation");
+header = document.getElementById("header");
+
+function changeNavigation() {
+  navigation.classList.toggle("visible");
+  header.classList.toggle("open");
+}
