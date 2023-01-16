dbb0abfa60928751db2a2399ae928e9ff2681170
Wragnardev <shalithamza@gmail.com>
2023-01-16 21:43:11 +0300

    push

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,9 @@
 navigation = document.getElementById("navigation");
 header = document.getElementById("header");
+backgroundVideo = document.getElementById("backgroundVideo");
 
 function changeNavigation() {
   navigation.classList.toggle("visible");
   header.classList.toggle("open");
+  backgroundVideo.classList.toggle("fixed")
 }
